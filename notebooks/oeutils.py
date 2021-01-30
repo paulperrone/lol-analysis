350986692ef35ad00c14e8fe41d2c163e50f48a3
Paul Perrone <paulperrone@pop-os.localdomain>
2021-01-30 13:05:09 -0800

    add trueskill

--- notebooks/oeutils.py
+++ notebooks/oeutils.py
@@ -1,44 +1,48 @@
 # Housekeeping
+import glob
 import requests
 import datetime
 import pandas as pd
 import io
 import os
 
+# Variables
+current_date = datetime.date.today()
+current_year = current_date.year
+today = current_date.strftime('%Y%m%d')
+yesterday = current_date - datetime.timedelta(days = 1)
+yesterday = yesterday.strftime('%Y%m%d')
 
-def download(directory, years, delete=True):
+def download(directory, years=[current_year], delete=True):
     """
+    Downloads Oracle's Elixer .csv files to the given directory for the provided years.
+
     Parameters
     ----------
     directory : str
         A string containing the filepath to the working directory.
         (e.g. 'C:\\Users\\ProjektStation\\Documents\\OraclesElixir\\')
     year : str or list
-        A string or list of strings containing years (e.g. ["2019", "2020"])
+        A string or list of strings containing years (e.g. ["2019", "2020"]) 
     delete : boolean
         A boolean (True/False) value. 
         If True, will delete files in directory upon download of new data.
 
     Returns
     -------
-    A Pandas dataframe containing the most recent Oracle's Elixir data 
-    for the years provided by the year parameter.
-    A .csv file in the directory, with the most recent data, if downloaded.
+    None
     """
-    # Defining Time Variables
-    current_date = datetime.date.today()
-    today = current_date.strftime('%Y%m%d')
-    yesterday = current_date - datetime.timedelta(days = 1)
-    yesterday = yesterday.strftime('%Y%m%d')
-    
-    # Other Variables
+    # Variables
     url = ('https://oracleselixir-downloadable-match-data.'
         's3-us-west-2.amazonaws.com/')
-    oe_data = pd.DataFrame()
-    
+
     # Conditional Handling For Years
-    if isinstance(years, str):
-        years = [years]
+    if isinstance(years, list) == False:
+        listed_years = [years]
+        years = []
+        for year in listed_years:
+            if isinstance(year, int):
+                years.append(str(year))
     
     # Dynamic Data Import
     for year in years:
@@ -58,6 +62,7 @@ def download(directory, years, delete=True):
                 data = r.content.decode('utf8')
                 data = pd.read_csv(io.StringIO(data))
                 data.to_csv(f'{directory}{file}{today}.csv', index=False)
+                print('Oracle\'s Elixer download successful')
                 
             except:
                 # Grab Yesterday's Data If Today's Does Not Exist
@@ -67,17 +72,58 @@ def download(directory, years, delete=True):
                 data = r.content.decode('utf8')
                 data = pd.read_csv(io.StringIO(data))
                 data.to_csv(f'{directory}{file}{yesterday}.csv', index=False)
+                print('Oracle\'s Elixer download successful')
 
 # Update to read only the most recent file for a single year
-def read(directory):
+def read(directory, years=[current_year]):
+    """
+    Returns a dataframe with the Oracles Elixer data in the provided directory for the given years.
+
+    Parameters
+    ----------
+    directory : str
+        A string containing the filepath to the working directory.
+        (e.g. 'C:\\Users\\ProjektStation\\Documents\\OraclesElixir\\')
+    year : str or list
+        A string or list of strings containing years (e.g. ["2019", "2020"]) 
+
+    Returns
+    -------
+    A Pandas dataframe containing the most recent Oracle's Elixir data 
+    for the years provided by the year parameter.
+    """
     oe_data = pd.DataFrame()
-    for csv in os.listdir(directory):
-        data = pd.read_csv(f'{directory}{csv}')
-        oe_data = pd.concat([oe_data, data], axis=0)
+    
+    # Conditional Handling For Years
+    if isinstance(years, list) == False:
+        listed_years = [years]
+        years = []
+        for year in listed_years:
+            if isinstance(year, int):
+                years.append(str(year))
+    
+    # Dynamic Data Import
+    for year in years:
+        try:
+            prefix_path = f'{directory}{year}_LoL_esports_match_data_from_OraclesElixir_'
+            glob_path = prefix_path + '*.csv'
+            matching_files = glob.glob(glob_path)
+            most_recent_file_date = 0
+            for match in matching_files:
+                if int(match[-12:-4]) > most_recent_file_date:
+                    most_recent_file_date = int(match[-12:-4])
+            import_date = str(most_recent_file_date)
+            data = pd.read_csv(f'{prefix_path}{import_date}.csv')
+            oe_data = pd.concat([oe_data, data], axis=0)
+        except:
+            print(f'OE Data for {year} not found.')
+
     return oe_data
 
 def clean(oe_data, split_on, keep_identities, keep_leagues, keep_columns):
     """
+    Accpets, cleans to the input parameters, and returns a dataframe with Oracle's Elixer data.
+
     Parameters
     ----------
     oe_data : DataFrame
